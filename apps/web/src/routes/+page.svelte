9484925e9f64f0a6e692dcb9754addc99e1805c1
dlmr <lemaire.dylan@proton.me>
2026-06-11 02:19:10 +0200

    refactor layout and styling in +page.svelte for improved structure and theme toggle

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -15,24 +15,24 @@
 </script>
 
 <div
-	class="container mx-auto flex h-dvh flex-col bg-background p-4 text-foreground transition-colors duration-300"
+	class="container mx-auto flex h-dvh flex-col gap-4 bg-background p-4 text-foreground transition-colors duration-300"
 >
-	<div class="flex flex-row justify-center">
-		<h1 class="text-center text-4xl font-bold">Janety</h1>
-		<Button onclick={toggleMode} variant="outline" size="icon" class="absolute right-0">
+	<div class="flex flex-row items-center justify-center">
+		<h1 class="flex-1 text-center text-4xl font-bold">Janety</h1>
+		<Button onclick={toggleMode} variant="outline" size="icon">
 			<Sun
-				class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
+				class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
 			/>
 			<Moon
-				class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
+				class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
 			/>
 			<span class="sr-only">Toggle theme</span>
 		</Button>
 	</div>
 
-	<div class="flex h-full flex-1 flex-col gap-4 md:flex-row">
-		<div class="flex flex-col md:flex-1">
-			<Label for="janety-editor" class="mb-2 block shrink-0 text-xl">Janety</Label>
+	<div class="flex flex-1 flex-col gap-4 md:flex-row">
+		<div class="flex min-h-full shrink-0 flex-col md:min-h-0 md:flex-1">
+			<Label for="janety-editor" class="mb-2 block shrink-0 text-center text-xl">Janety</Label>
 
 			<Editor
 				bind:value={janetyText}
@@ -42,8 +42,8 @@
 			/>
 		</div>
 
-		<div class="flex flex-col md:flex-1">
-			<Label for="janet-editor" class="mb-2 block shrink-0 text-xl">Janet</Label>
+		<div class="flex min-h-full pb-4 md:pb-0 shrink-0 flex-col md:min-h-0 md:flex-1">
+			<Label for="janet-editor" class="mb-2 block shrink-0 text-center text-xl">Janet</Label>
 
 			<Editor
 				value={janetText}
