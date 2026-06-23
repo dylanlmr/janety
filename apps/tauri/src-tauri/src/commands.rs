3c9690ce3b48214d1a4c67246e57cd932e5cccf9
dlmr <lemaire.dylan@proton.me>
2026-06-23 22:36:36 +0200

    feat: update commands to use JanetyResult for improved error handling and add run_code and compile_and_run_code functions

--- apps/tauri/src-tauri/src/commands.rs
+++ apps/tauri/src-tauri/src/commands.rs
@@ -1,6 +1,16 @@
-use janety_core::CompileResult;
+use janety_core::JanetyResult;
 
 #[tauri::command]
-pub fn compile_code(code: &str) -> CompileResult {
+pub fn compile_code(code: &str) -> JanetyResult {
     janety_core::compile(code)
+}
+
+#[tauri::command]
+pub fn run_code(code: &str) -> JanetyResult {
+    janety_core::run(code, "tauri")
+}
+
+#[tauri::command]
+pub fn compile_and_run_code(code: &str) -> JanetyResult {
+    janety_core::compile_and_run(code, "tauri")
 }
